05e3f51e6b7cc5e35230e35f74fdf3e91bfb17aa
dharshiniudayakumar24-web <dharshiniudayakumar24@gmail.com>
2025-09-05 09:16:34 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,6 @@
+# Dharshini31 portfolio 
+
+A Pen created on CodePen.
+
+Original URL: [https://codepen.io/24AI131-Dharshiniu/pen/empxZGp](https://codepen.io/24AI131-Dharshiniu/pen/empxZGp).
+
---
+++ script.js
@@ -0,0 +1,71 @@
+// Highlight active navbar + Skills animation
+
+const sections = document.querySelectorAll("section");
+
+const navLinks = document.querySelectorAll("nav ul li a");
+
+const skillFills = document.querySelectorAll(".fill");
+
+const skillSection = document.querySelector("#skills");
+
+window.addEventListener("scroll", () => {
+
+  let current = "";
+
+  // Navbar active
+
+  sections.forEach(section => {
+
+    const sectionTop = section.offsetTop - 100;
+
+    if (scrollY >= sectionTop) {
+
+      current = section.getAttribute("id");
+
+    }
+
+  });
+
+  navLinks.forEach(link => {
+
+    link.classList.remove("active");
+
+    if (link.getAttribute("href") === "#" + current) {
+
+      link.classList.add("active");
+
+    }
+
+  });
+
+  // Skills animation
+
+  const sectionPos = skillSection.getBoundingClientRect().top;
+
+  const screenPos = window.innerHeight / 1.3;
+
+  if (sectionPos < screenPos) {
+
+    skillFills.forEach(fill => {
+
+      fill.style.width = fill.getAttribute("data-width");
+
+    });
+
+  }
+
+});
+
+// Contact form alert
+
+const form = document.getElementById("contactForm");
+
+form.addEventListener("submit", function(e) {
+
+  e.preventDefault();
+
+  alert("Thank you! Your details have been submitted.");
+
+  form.reset();
+
+});
